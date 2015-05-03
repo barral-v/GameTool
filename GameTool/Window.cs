89162530941baaf1b52cf665ba6da5b94cefe4fe
barral-v <valentin.barral@epitech.eu>
2015-05-03 13:39:29 +0800

    Finish window events (no encapsulation)

--- GameTool/Window.cs
+++ GameTool/Window.cs
@@ -24,6 +24,55 @@ namespace GameTool
             }
         }
 
+        public void onClose(dynamic callback)
+        {
+            Game.MainWindow.Closed += new EventHandler(delegate(object sender, EventArgs e) {
+                callback(e);
+            });
+        }
+
+        public void onGainedFocus(dynamic callback)
+        {
+            Game.MainWindow.GainedFocus += new EventHandler(delegate(object sender, EventArgs e) {
+                callback(e);
+            });
+        }
+
+        public void onJoystickButtonPressed(dynamic callback)
+        {
+            Game.MainWindow.JoystickButtonPressed += new EventHandler<JoystickButtonEventArgs>(delegate(object sender, JoystickButtonEventArgs e) {
+                callback(e);
+            });
+        }
+
+        public void onJoystickButtonReleased(dynamic callback)
+        {
+            Game.MainWindow.JoystickButtonReleased += new EventHandler<JoystickButtonEventArgs>(delegate(object sender, JoystickButtonEventArgs e) {
+                callback(e);
+            });
+        }
+
+        public void onJoystickConnected(dynamic callback)
+        {
+            Game.MainWindow.JoystickConnected += new EventHandler<JoystickConnectEventArgs>(delegate(object sender, JoystickConnectEventArgs e) {
+                callback(e);
+            });
+        }
+
+        public void onJoystickDisconnected(dynamic callback)
+        {
+            Game.MainWindow.JoystickDisconnected += new EventHandler<JoystickConnectEventArgs>(delegate(object sender, JoystickConnectEventArgs e) {
+                callback(e);
+            });
+        }
+
+        public void onJoystickMoved(dynamic callback)
+        {
+            Game.MainWindow.JoystickMoved += new EventHandler<JoystickMoveEventArgs>(delegate(object sender, JoystickMoveEventArgs e) {
+                callback(e);
+            });
+        }
+
         public void onKeyPress(dynamic callback)
         {
             Game.MainWindow.KeyPressed += new EventHandler<KeyEventArgs>(delegate(object sender, KeyEventArgs e) {
@@ -32,9 +81,102 @@ namespace GameTool
             });
         }
 
-        public void onClose(dynamic callback)
+        public void onKeyReleased(dynamic callback)
         {
-            Game.MainWindow.Closed += new EventHandler(delegate(object sender, EventArgs e) {
+            Game.MainWindow.KeyReleased += new EventHandler<KeyEventArgs>(delegate(object sender, KeyEventArgs e) {
+                callback(e);
+            });
+        }
+
+        public void onLostFocus(dynamic callback)
+        {
+            Game.MainWindow.LostFocus += new EventHandler(delegate(object sender, EventArgs e) {
+                callback(e);
+            });
+        }
+
+        public void onMouseButtonPressed(dynamic callback)
+        {
+            Game.MainWindow.MouseButtonPressed += new EventHandler<MouseButtonEventArgs>(delegate(object sender, MouseButtonEventArgs e) {
+                callback(e);
+            });
+        }
+
+        public void onMouseButtonReleased(dynamic callback)
+        {
+            Game.MainWindow.MouseButtonReleased += new EventHandler<MouseButtonEventArgs>(delegate(object sender, MouseButtonEventArgs e) {
+                callback(e);
+            });
+        }
+
+        public void onMouseEntered(dynamic callback)
+        {
+            Game.MainWindow.MouseEntered += new EventHandler(delegate(object sender, EventArgs e) {
+                callback(e);
+            });
+        }
+
+        public void onMouseLeft(dynamic callback)
+        {
+            Game.MainWindow.MouseLeft += new EventHandler(delegate(object sender, EventArgs e) {
+                callback(e);
+            });
+        }
+
+        public void onMouseMoved(dynamic callback)
+        {
+            Game.MainWindow.MouseMoved += new EventHandler<MouseMoveEventArgs>(delegate(object sender, MouseMoveEventArgs e) {
+                callback(e);
+            });
+        }
+
+        public void onMouseWheelMoved(dynamic callback)
+        {
+            Game.MainWindow.MouseWheelMoved += new EventHandler<MouseWheelEventArgs>(delegate(object sender, MouseWheelEventArgs e) {
+                callback(e);
+            });
+        }
+
+        public void onResized(dynamic callback)
+        {
+            Game.MainWindow.Resized += new EventHandler<SizeEventArgs>(delegate(object sender, SizeEventArgs e) {
+                callback(e);
+            });
+        }
+
+        public void onSensorChanged(dynamic callback)
+        {
+            Game.MainWindow.SensorChanged += new EventHandler<SensorEventArgs>(delegate(object sender, SensorEventArgs e) {
+                callback(e);
+            });
+        }
+
+        public void onTextEntered(dynamic callback)
+        {
+            Game.MainWindow.TextEntered += new EventHandler<TextEventArgs>(delegate(object sender, TextEventArgs e) {
+                callback(e);
+            });
+        }
+
+        public void onTouchBegan(dynamic callback)
+        {
+            Game.MainWindow.TouchBegan += new EventHandler<TouchEventArgs>(delegate(object sender, TouchEventArgs e) {
+                callback(e);
+            });
+        }
+
+        public void onTouchEnded(dynamic callback)
+        {
+            Game.MainWindow.TouchEnded += new EventHandler<TouchEventArgs>(delegate(object sender, TouchEventArgs e)
+            {
+                callback(e);
+            });
+        }
+
+        public void onTouchMoved(dynamic callback)
+        {
+            Game.MainWindow.TouchMoved += new EventHandler<TouchEventArgs>(delegate(object sender, TouchEventArgs e)
+            {
                 callback(e);
             });
         }
